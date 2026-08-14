a119faf50d122f5fd3efb693af606219d5045df5
AnandhaSivam-00 <anandsugan218@gmail.com>
2026-08-14 14:59:19 +0530

    chore: update the import styling for the .env properties

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -12,9 +13,5 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-  ],
-  define: {
-    'process.env.VITE_GOOGLE_GEMINI_MODEL_API': JSON.stringify(process.env.VITE_GOOGLE_GEMINI_MODEL_API),
-    'process.env.VITE_HUGGING_FACE_MISTRAL_MODEL_API': JSON.stringify(process.env.VITE_HUGGING_FACE_MISTRAL_MODEL_API)
-  }
+  ]
 })
